447b076c00621cedbece746a45b73f99e1a55f9b
Gerardo Gómez B <gerardogomezbrenes2112@gmail.com>
2024-10-25 12:58:55 -0600

    CORS enabled all origins

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers import pharmacy_router, user_router, product_router, request_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(name="API Diseño de Software")
 
@@ -11,3 +12,12 @@ app.include_router(request_router)
 @app.get("/")
 def is_running():
     return {"running": True}
+
+# Permitir todos los orígenes
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
